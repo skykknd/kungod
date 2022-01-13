9a52509c3a2e91d989466042454e4da21e9fc814
skykknd <76689701@qq.com>
2022-01-13 09:56:54 +0800

    Add test code.

--- app/src/main/java/com/sky/kungod/MainActivity.java
+++ app/src/main/java/com/sky/kungod/MainActivity.java
@@ -1,7 +1,6 @@
 package com.sky.kungod;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
@@ -20,22 +19,10 @@ public class MainActivity extends Activity {
     private void init() {
         KunGod.init(getApplicationContext());
         kunGod = KunGod.get();
-
-        kunGod.scan(1001);
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return kunGod.exitConfirm(keyCode, event);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1001 && resultCode == Activity.RESULT_OK) {
-            if (data != null) {
-                kunGod.sToast(data.toString());
-            }
-        }
     }
 }
